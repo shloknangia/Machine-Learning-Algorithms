435358939c312c7a9416930e57176856e72ae0cd
Shlok Nangia <shloknangia@gmail.com>
2017-12-08 19:26:46 +0530

    datadet_ques and regression updated

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -19,4 +19,67 @@ import pickle
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
 
+print len(enron_data)
 
+print len(enron_data["SKILLING JEFFREY K"])
+
+count = 0
+for n in enron_data:
+	if enron_data[n]["poi"] == 1:
+		count += 1
+print count
+
+
+poi = open("../final_project/poi_names.txt")
+c = 0
+for a in poi:
+	c += 1
+print c-2
+
+for v in enron_data["PRENTICE JAMES"]:
+	print v
+print enron_data["PRENTICE JAMES"]["total_stock_value"]
+
+print enron_data["COLWELL WESLEY"]["from_this_person_to_poi"]
+
+print enron_data["SKILLING JEFFREY K"]["exercised_stock_options"]
+	
+print enron_data["SKILLING JEFFREY K"]["total_payments"]
+print enron_data["LAY KENNETH L"]["total_payments"]
+print enron_data["FASTOW ANDREW S"]["total_payments"]
+
+print enron_data["FASTOW ANDREW S"]
+
+sal = 0
+email_add = 0
+for i in enron_data:
+	if enron_data[i]["salary"] != 'NaN':
+		sal += 1
+	if enron_data[i]["email_address"] != 'NaN':
+		email_add += 1
+
+print sal, email_add
+
+tp1 = 0
+for i in enron_data:
+	if enron_data[i]["total_payments"] == 'NaN':
+		tp1 += 1
+
+total = 0
+for j in enron_data:
+	total += 1
+
+print (tp1*100.0)/total
+
+tp = 0
+for i in enron_data:
+	if enron_data[i]["total_payments"] == 'NaN' and enron_data[i]["poi"] == True:
+		tp += 1
+
+total = 0
+for j in enron_data:
+	total += 1
+
+print (tp*100.0)/total
+
+print total,tp1,tp,count
