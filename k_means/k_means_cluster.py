738641ba8d06a289810171367ac65eea81f2e5c5
Shlok Nangia <shloknangia@gmail.com>
2017-12-14 16:05:03 +0530

    Lesson 8 Clustering

--- k_means/k_means_cluster.py
+++ k_means/k_means_cluster.py
@@ -43,11 +43,24 @@ data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r")
 ### there's an outlier--remove it! 
 data_dict.pop("TOTAL", 0)
 
+maxsal = 0
+minsal = 10000000000
+for i in data_dict:
+    s = data_dict[i]['salary']
+    if s!='NaN':
+        if s>maxsal:
+            maxsal = s
+        if s<minsal:
+            minsal = s
+
+print minsal, maxsal            
+
 
 ### the input features we want to use 
 ### can be any key in the person-level dictionary (salary, director_fees, etc.) 
 feature_1 = "salary"
 feature_2 = "exercised_stock_options"
+# feature_3 = "total_payments"
 poi  = "poi"
 features_list = [poi, feature_1, feature_2]
 data = featureFormat(data_dict, features_list )
@@ -59,11 +72,14 @@ poi, finance_features = targetFeatureSplit( data )
 ### for f1, f2, _ in finance_features:
 ### (as it's currently written, the line below assumes 2 features)
 for f1, f2 in finance_features:
-    plt.scatter( f1, f2 )
+    plt.scatter( f1, f2)
 plt.show()
 
 ### cluster here; create predictions of the cluster labels
 ### for the data and store them to a list called pred
+
+from sklearn.cluster import KMeans
+pred = KMeans(n_clusters = 2).fit(finance_features).predict(finance_features)
 
 
 
@@ -71,6 +87,6 @@ plt.show()
 ### rename the "name" parameter when you change the number of features
 ### so that the figure gets saved to a different file
 try:
-    Draw(pred, finance_features, poi, mark_poi=False, name="clusters.pdf", f1_name=feature_1, f2_name=feature_2)
+    Draw(pred, finance_features, poi, mark_poi=False, name="clusters2.pdf", f1_name=feature_1, f2_name=feature_2)
 except NameError:
     print "no predictions object named pred found, no clusters to plot"
