61dd3f60ca5cdf756f662ae231106b61bbed8802
Shlok Nangia <shloknangia@gmail.com>
2017-12-19 17:42:24 +0530

    Lesson 12--pca--completed

--- pca/eigenfaces.py
+++ pca/eigenfaces.py
@@ -144,3 +144,6 @@ eigenface_titles = ["eigenface %d" % i for i in range(eigenfaces.shape[0])]
 plot_gallery(eigenfaces, eigenface_titles, h, w)
 
 pl.show()
+
+print pca.explained_variance_ratio_[0]
+print pca.explained_variance_ratio_[1]
