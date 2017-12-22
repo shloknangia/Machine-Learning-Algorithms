a7d4c1a7aa055bff23e866d3c39aeae599779c65
Shlok Nangia <shloknangia@gmail.com>
2017-12-22 17:51:26 +0530

    Lesson-14 -- Evaluation -- Completed

--- evaluation/evaluate_poi_identifier.py
+++ evaluation/evaluate_poi_identifier.py
@@ -28,4 +28,32 @@ labels, features = targetFeatureSplit(data)
 
 ### your code goes here 
 
+from sklearn.tree import DecisionTreeClassifier
+clf = DecisionTreeClassifier()
+clf.fit(features, labels)
+print clf.score(features, labels)
 
+from sklearn.cross_validation import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(features, labels, test_size = 0.3, random_state = 42)
+clf.fit(x_train, y_train)
+print clf.score(x_test, y_test)
+print x_test, y_test
+print len(x_test), len(y_test)
+
+pred = clf.predict(x_test)
+count = 0
+for i in range(len(y_test)):
+	if pred[i] == y_test[i]:
+		count += 1
+
+print count		
+
+from sklearn import metrics
+print metrics.precision_score(y_test, pred)
+print metrics.recall_score(y_test, pred)
+
+predictions = [0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1, 0, 1] 
+true_labels = [0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 0]	
+print metrics.confusion_matrix(predictions, true_labels)
+print metrics.precision_score(true_labels, predictions)
+print metrics.recall_score(true_labels, predictions)
